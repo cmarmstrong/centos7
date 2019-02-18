bc9882a06866764b39c8957ce65496fea188fd83
Chandler M. Armstrong <cmarmstrong@cerrrb-wn-t1396.erdc.dren.mil>
2019-02-18 15:30:29 -0500

    revis(ing) anaconda section in .sh, added local pkgs in .R

--- main.R
+++ main.R
@@ -3,6 +3,17 @@ options(unzip='internal') # for devtools::install_github
 library(devtools)
 devtools::install_github('r-lib/devtools')
 
-stop() # comment to install local
-devtools::install_local('remdat', upgrade_dependencies=FALSE)
-# ripums, rgtd, etc
+## stop() # comment to install others
+install.packages(c('rnaturalearth', 'tigris'))
+BiocInstaller::bioclite(c('aroma.light'))
+devtools::install_local('rv', dependencies=FALSE)
+devtools::install_local('raccess', dependencies=FALSE)
+devtools::install_local('rlandscan', dependencies=FALSE)
+devtools::install_local('rsoils', dependencies=FALSE)
+devtools::install_local('rspam', dependencies=FALSE)
+devtools::install_local('rvisnav', dependencies=FALSE)
+devtools::install_local('rworldclim', dependencies=FALSE)
+devtools::install_local('remdat', dependencies=FALSE)
+devtools::install_local('rgtd', dependencies=FALSE)
+devtools::install_local('rnino', dependencies=FALSE)
+devtools::install_local('ripums', dependencies=FALSE)
--- main.sh
+++ main.sh
@@ -33,7 +33,11 @@ bash Miniconda2-latest-Linux-x86_64.sh
 conda config --add channels conda-forge
 # CRAN versions of sf and rpostgresql don't work with anaconda?
 # NOTE: this is using system compilers; only way to use anaconda compilers is to write package?
-conda install gcc_linux-64 gxx_linux-64 gfortran_linux-64 gdal R r-essentials r-sf r-rpostgresql r-rserve
+conda install -y gcc_linux-64 gxx_linux-64 gfortran_linux-64
+conda install -y "gdal>2.2.4" # -c conda-forge --override-channels installs python3 breaking conda
+conda install -y R r-essentials r-sf r-rpostgresql r-rserve
+conda install -y r-countrycode r-gdalUtils r-rgeos r-geosphere r-gstat r-raster r-sp # local deps
+# not in conda: rnaturalearth tigris; see main.R
 # mkdir ~/Desktop/tmp
 # export TMPDIR=~/Desktop/tmp # if noexec causes trouble
 
